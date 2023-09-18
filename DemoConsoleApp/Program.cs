fefc6c633a0467b544a29fd6e34299b1317a9e0c
skowront <tomasz.skowron98@gmail.com>
2023-09-18 11:49:29 +0200

    Add project files.

---
+++ DemoConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DemoConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = true;
+            while (true)
+            {
+                Program.Work(x);
+                x = !x;
+            }
+        }
+
+        public static void Work(bool x)
+        {
+            Console.WriteLine("Working: "+x.ToString());
+        }
+    }
+}
